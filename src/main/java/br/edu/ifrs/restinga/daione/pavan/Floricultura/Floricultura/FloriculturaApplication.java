3d4e11bd21b0f4e7a4a89e60c0645d274cddf9a7
DaioneB <dcbpavan@restinga.ifrs.edu.br>
2018-11-27 23:09:10 -0200

    primeiro commit

---
+++ src/main/java/br/edu/ifrs/restinga/daione/pavan/Floricultura/Floricultura/FloriculturaApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifrs.restinga.daione.pavan.Floricultura.Floricultura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloriculturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloriculturaApplication.class, args);
+	}
+}
